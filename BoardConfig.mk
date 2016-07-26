1ad3aaaf213e24b63e348c3e65044b0ae9004c1f
SpiritCroc <spiritcroc@gmail.com>
2016-07-26 11:48:31 +0200

    More DU instead of Omni

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/rhine-common/PlatformConfigOmni.mk
+include device/sony/rhine-common/PlatformConfigDU.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := D5503
 
